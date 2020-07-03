3b9a9cc06cefd96937c197d33aa3d79f2fcfdecd
C. E. Sharp <chrisesharp@gmail.com>
2020-07-03 17:23:51 +0100

    rudimentary targetting

--- src/monsters/goblin-brain.js
+++ src/monsters/goblin-brain.js
@@ -2,6 +2,17 @@
 
 import Brain from './brain.js';
 
+function distance(pos1, pos2) {
+    return Math.floor(Math.sqrt((pos1.x - pos2.x)**2 + (pos1.y - pos2.y)**2));
+}
+
+function keyToPos(key, z) {
+    let parts = key.split(',');
+    let x = parts[0].slice(1);
+    let y = parts[1].split(')')[0];
+    return {x:x, y:y, z:z};
+}
+
 export default class GoblinBrain extends Brain {
     ready(event) {
         if (event === 'entities') {
@@ -10,9 +21,15 @@ export default class GoblinBrain extends Brain {
     }
 
     findTarget() {
+        let goblin = this.client.getParticipant();
         let target;
+        let closest = goblin.getSightRadius();
         Object.keys(this.client.others).forEach(key => {
-            target = this.client.others[key];
+            let dist = distance(goblin.pos, keyToPos(key, goblin.pos.z));
+            if (dist <= closest) {
+                closest = dist;
+                target = this.client.others[key];
+            }
         });
         return target;
     }
--- test/brain.test.js
+++ test/brain.test.js
@@ -4,22 +4,38 @@ import GoblinBrain from "../src/monsters/goblin-brain";
 import Cave from "../src/server/cave";
 import Goblin from "../src/server/entities/goblin";
 import Warrior from "../src/server/entities/warrior";
+import Wizard from "../src/server/entities/wizard";
 
 const defaultMap = {
     "width":4,
     "height":5,
   };
 
-const warrior = new Warrior({pos:{x:2,y:2,z:0}});
+const warrior = new Warrior({pos:{x:3,y:3,z:0}});
+const wizard = new Wizard({pos:{x:2,y:2,z:0}});
 const goblin = new Goblin({pos:{x:0,y:0,z:0}});
 
 describe('goblin brain responses', () => {
     test('should identify targets', (done) => {
         let map = new Cave(defaultMap).getMap();
         let client = {
-            participant: goblin,
+            getParticipant: () => { return goblin;},
             entities: {"(2,2)": warrior, "(0,0)": goblin },
             others: {"(2,2)": warrior}
+        };
+        let messages = [];
+        let brain = new GoblinBrain(map, client, messages);
+        brain.ready("entities");
+        expect(brain.getCurrentTarget()).toEqual(warrior);
+        done();
+    });
+
+    test('should identify closest target', (done) => {
+        let map = new Cave(defaultMap).getMap();
+        let client = {
+            getParticipant: () => { return goblin;},
+            entities: {"(2,2)": warrior, "(3,3)": wizard, "(0,0)": goblin },
+            others: {"(2,2)": warrior, "(3,3)": wizard}
         };
         let messages = [];
         let brain = new GoblinBrain(map, client, messages);
