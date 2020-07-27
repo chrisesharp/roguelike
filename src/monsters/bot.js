edceb52ce6afdb982d7d290bfc780d266191b9ed
C. E. Sharp <chrisesharp@gmail.com>
2020-07-27 16:28:06 +0100

    refactor bot into base class

---
+++ src/monsters/bot.js
@@ -0,0 +1,61 @@
+"use strict";
+
+import RogueClient from '../client/rogue-client.js';
+import Map from '../common/map.js';
+import { EVENTS } from '../common/events.js';
+
+export default class Bot {
+    constructor(URL, brain) {
+        this.serverAddr = URL;
+        this.messages = [];
+        this.client = new RogueClient(this.serverAddr, (event, data) => {this.refresh(event, data);});
+        this.brain = brain;
+        this.startPos = null;
+    }
+
+    start(props, callback) {
+        if (callback) {
+            this.brain.ready = callback;
+        }
+        props.type = 'monster';
+        this.client.connectToServer(props)
+        return this;
+    }
+
+    ready(event, data) {
+        this.brain.ready(event, data);
+    }
+
+    stop() {
+        this.client.disconnectFromServer();
+    }
+ 
+    mapAvailable(data) {
+        this.brain.setMap(new Map(data));
+    }
+
+    refresh(event, data) {
+        if (event === EVENTS.message) {
+            this.addMessage(data);
+        }
+
+        if (event === EVENTS.map) {
+            this.mapAvailable(data);
+        }
+        this.ready(event, data);
+    }
+
+    addMessage(messages) {
+        if (messages instanceof Array) {
+            messages.forEach((message) => {
+                this.messages.push(message);
+            });
+        } else {
+            this.messages.push(messages);
+        }
+    }
+
+    move(direction) {
+        this.client.move(direction);
+    }
+}
--- src/monsters/goblin-bot.js
+++ src/monsters/goblin-bot.js
@@ -1,74 +1,27 @@
 "use strict";
 
-import RogueClient from '../client/rogue-client.js';
-import Map from '../common/map.js';
+import Bot from './bot.js';
 import GoblinBrain from './goblin-brain.js';
 import Goblin from '../server/entities/Goblin.js';
-import { EVENTS } from '../common/events.js';
 
-export default class GoblinBot {
+export default class GoblinBot extends Bot {
     static numberOccuring = 3;
     static name = 'Goblin';
     static role = 'goblin';
 
     constructor(URL, brain) {
-        this.serverAddr = URL;
-        this.messages = [];
-        this.client = new RogueClient(this.serverAddr, (event, data) => {this.refresh(event, data);});
+        super(URL, brain);
         this.brain = brain || new GoblinBrain(null, this.client, this.messages);
         this.role = GoblinBot.role;
-        this.startPos = null;
     }
 
     start(startPos, callback) {
-        if (callback) {
-            this.brain.ready = callback;
-        }
         let props =  {
             name: GoblinBot.name,
-            role: GoblinBot.role,
-            type: 'monster'
+            role: this.role
           };
         this.startPos =  (startPos) ? startPos : {z:Goblin.level};
         props.pos = JSON.stringify(this.startPos)
-        this.client.connectToServer(props)
-        return this;
-    }
-
-    ready(event, data) {
-        this.brain.ready(event, data);
-    }
-
-    stop() {
-        this.client.disconnectFromServer();
-    }
- 
-    mapAvailable(data) {
-        this.brain.setMap(new Map(data));
-    }
-
-    refresh(event, data) {
-        if (event === EVENTS.message) {
-            this.addMessage(data);
-        }
-
-        if (event === EVENTS.map) {
-            this.mapAvailable(data);
-        }
-        this.ready(event, data);
-    }
-
-    addMessage(messages) {
-        if (messages instanceof Array) {
-            messages.forEach((message) => {
-                this.messages.push(message);
-            });
-        } else {
-            this.messages.push(messages);
-        }
-    }
-
-    move(direction) {
-        this.client.move(direction);
+        return super.start(props, callback);
     }
 }
