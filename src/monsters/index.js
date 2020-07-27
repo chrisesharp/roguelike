f9e1c35932b67c2af4e11375cda78d07e94f9c75
C. E. Sharp <chrisesharp@gmail.com>
2020-07-27 11:55:45 +0100

    dynamic monster generation

---
+++ src/monsters/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const Bots = {};
+
+import GoblinBot from './goblin-bot.js';
+Bots.GoblinBot = GoblinBot;
+
+export { Bots };
--- src/start-monsters.js
+++ src/start-monsters.js
@@ -1,11 +1,18 @@
 "use strict";
 
 import io from 'socket.io-client';
-import GoblinBot from './monsters/goblin-bot.js';
+import { Bots } from './monsters/index.js';
 import { EVENTS } from './common/events.js';
 
 const serverAddr = process.env.server || "http://0.0.0.0:3000";
+
 const monsters = [];
+Object.keys(Bots).forEach(key => {
+    let ctor = Bots[key];
+    monsters.push({type:ctor, frequency: Math.floor(Math.random()*4)+1})
+  });
+
+const live = [];
 
 let socket = io(serverAddr, {
     'reconnection delay': 0,
@@ -14,19 +21,23 @@ let socket = io(serverAddr, {
     transports: ['websocket'],
 });
 
-console.log("Starting monsters...");
 socket.on("connect",() => {
-    console.log("...now we're connected to server.");
+    console.log("now we're connected to server...");
 })
 
 socket.once(EVENTS.missingRole, (error) => {
+    console.log("...we can start the monsters...");
     addMonsters(serverAddr);
     socket.disconnect();
 });
 
 function addMonsters(URL) {
-    for (let i = 0; i < 3; i++) {
-        monsters.push(new GoblinBot(URL).start());
-        console.log("Started goblin ",i);
-    }
+    monsters.forEach(entry => {
+        let monster = entry.type;
+        let freq = entry.frequency;
+        for (let i=1; i <= freq; i++) {
+            live.push(new monster(URL).start());
+            console.log(`Started goblin (${i}/${freq})`);
+        }
+    });
 }
