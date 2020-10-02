4df6334044ea71a8273a6294a8fb2aa422692067
C. E. Sharp <chrisesharp@gmail.com>
2020-10-02 15:19:53 +0100

    deps for react usage

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,18 @@
 module.exports = {
     presets: [
       [
+        "@babel/preset-react",
+        {
+          // pragma: "dom", // default pragma is React.createElement (only in classic runtime)
+          // pragmaFrag: "DomFrag", // default is React.Fragment (only in classic runtime)
+          // throwIfNamespace: true, // defaults to true
+          runtime: "automatic", // defaults to classic
+          development: true,
+          // useBuiltIns: true,
+          // "importSource": "custom-jsx-library" // defaults to react (only in automatic runtime)
+        }
+      ],
+      [
         '@babel/preset-env',
         {
           targets: {
@@ -9,4 +21,9 @@ module.exports = {
         },
       ],
     ],
+    plugins: [
+      "@babel/plugin-syntax-jsx",
+      "@babel/plugin-transform-react-jsx",
+      "@babel/plugin-transform-react-display-name",
+    ]
   };
