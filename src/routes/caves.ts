1cd541e66a8e2e2246d7753408702b7d6b63697c
Chris Sharp <chrisesharp@gmail.com>
2021-08-13 09:01:33 +0100

    add logging

--- src/routes/caves.ts
+++ src/routes/caves.ts
@@ -1,6 +1,10 @@
 import { Application, Router } from 'express';
 import * as fs from 'fs';
 import * as process from 'process';
+import axios from 'axios';
+import { Logger } from '../common/logger';
+
+const log = new Logger();
 
 interface CaveConfig {
     cavepath?: string
@@ -12,6 +16,7 @@ type CaveEntry = {
     url: string
 }
 
+const serviceAddr = "http://cavern-service:3000";
 const filepath = process.env.CONFIG || process.env.npm_package_config_file || './src/server/config/defaults.json';
 const configFile = fs.readFileSync(filepath, 'utf8');
 const config: CaveConfig = JSON.parse(configFile);
@@ -28,7 +33,21 @@ caves.forEach(element => {
 export default function (app: Application): void {
     const router = Router();
     router.get('/', function (req, res) {
-        res.json(caves);
+        log.debug(`Receiving GET...our domain is ${process.env.DOMAIN}`);
+        if (process.env.DOMAIN && process.env.DOMAIN !== ':3000') {
+            log.debug(`Proxying GET to ${serviceAddr}`);
+            axios.get(`${serviceAddr}/caves`,{
+                timeout: 2500
+            }).then( (result) => {
+                res.json(result.data);
+            }).catch( (err) => {
+                log.debug("something went wrong ", err);
+                res.json(caves);
+            });
+        } else {
+            log.debug("Returning local file", serviceAddr);
+            res.json(caves);   
+        }
     });
 
     app.use("/caves", router);
