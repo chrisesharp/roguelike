1f3c68b2269e6348f07a8669eadf9565e8a3b844
Chris Sharp <chrisesharp@gmail.com>
2021-08-16 12:48:30 +0100

    add room prefix enablement to deployment

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -9,9 +9,9 @@ helm upgrade cavern-service cavern_server/ --install
 echo "Installing entrance"
 helm upgrade entrance entrance/ --install --set domain=${DOMAIN}
 echo "Installing cave1"
-helm upgrade cave1 default_cave/ --install
+helm upgrade cave1 default_cave/ --install --set caveid="cave1/"
 echo "Installing cave2"
-helm upgrade cave2 default_cave/ --install
+helm upgrade cave2 default_cave/ --install --set caveid="cave1/"
 echo "Installing monsters"
 helm upgrade monsters monsters/ --install --set server="http://cave1:3000"
 echo "Finished"
