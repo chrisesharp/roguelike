d81e0d00166bdcc4ef31bfe42e95d22cb91b8e30
Chris Sharp <chrisesharp@gmail.com>
2021-09-03 08:55:29 +0100

    small clean

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -3,22 +3,24 @@
 # Helm install various components of Kaverns & Kubernetes using environment variables to determine the deployed domain name
 
 export SVC_NAME=${SERVICENAME:-"caverns"}
+export CAVE_1="cave1"
+export CAVE_2="cave2"
 
-echo "Using ${SVC_NAME} as service name for caverns"
+echo "Using ${SVC_NAME} as name for caverns service"
 
 echo "Installing cavern-service"
 helm upgrade kandk cavern_server/ --install --set domain=${DOMAIN} --set caveid="entrance/" --set servicename=${SVC_NAME}
 
 echo "Installing cave1"
-helm upgrade cave1 default_cave/ --install --set caveid="cave1/"
+helm upgrade cave1 default_cave/ --install --set caveid="${CAVE_1}/"
 
 echo "Installing cave2"
-helm upgrade cave2 default_cave/ --install --set caveid="cave2/"
+helm upgrade cave2 default_cave/ --install --set caveid="${CAVE_2}/"
 
 echo "Installing monsters into cave 1"
-helm upgrade monsters1 monsters/ --install --set server="http://cave1:3000"
+helm upgrade monsters1 monsters/ --install --set server="http://${CAVE_1}:3000"
 
 echo "Installing monsters into cave 1"
-helm upgrade monsters2 monsters/ --install --set server="http://cave2:3000"
+helm upgrade monsters2 monsters/ --install --set server="http://${CAVE_2}:3000"
 
 echo "Finished"
